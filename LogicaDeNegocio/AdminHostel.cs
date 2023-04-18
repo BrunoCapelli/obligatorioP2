1397215bb2685d2fa6117feb5d5a192161da5964
Nicolas Herrera Aguete <nicolasherrera2014@outlook.com>
2023-04-18 18:44:19 -0300

    Agrego metodos de alta y busqueda de Actividades y Proveedores

--- LogicaDeNegocio/AdminHostel.cs
+++ LogicaDeNegocio/AdminHostel.cs
@@ -14,10 +14,10 @@ namespace LogicaDeNegocio
 
         #region Metodos
         // Validar que no exista un usuario con los datos ingresados
-        public bool ExisteUsuario(string nroDocumentoHuesped, TipoDocumento tipoDocumentoHuesped)
+        public bool ExisteUsuario(string nroDocumentoHuesped, TipoDocumento tipoDocumentoHuesped) //mejor que un existe es usar un Buscar
         {
             bool existe = false;
-            foreach (UsuarioHuesped user in _usuarios)
+            foreach (UsuarioHuesped user in _usuarios) //aca es mejor usar un while
             {
                 // Verifico que no exista otro huesped con este numero y tipo de documento
                 if (user.NroDocumento == nroDocumentoHuesped && user.TipoDoc == tipoDocumentoHuesped)
@@ -28,6 +28,105 @@ namespace LogicaDeNegocio
             return existe;
         }
         #endregion
+
+        public Proveedor BuscarProveedor(string nombre) {
+            Proveedor prov = null;
+            bool existe = false;
+            int i = 0;
+
+            while (int i < _proveedores.Count && existe ==false) { //lo hago con un while para que no recorra innecesariamente
+                if (_proveedores[i].NombreProveedor == nombre) {
+                    existe = true;
+                    prov = _proveedores[i];
+                }
+            }
+            
+            return prov;
+        }
+
+        public void AltaProveedor(string nombre,string telefonoProveedor,string direccionProveedor,int descuentoFijo) {
+            Proveedor prov = new Proveedor(nombre, telefonoProveedor, direccionProveedor, descuentoFijo);
+               
+            try {
+                prov.Validate(); //primero lo valido, si da error ya catchea la excepcion y no continua la ejecucion
+            
+                if (BuscarProveedor(nombre) == null) { //busco el proveedor, si da null es que no existe y puedo agregarlo
+                    _proveedores.Add(prov);
+                } else {  //si no da null arrojo excepcion
+                    throw new Exception("Ya existe un proveedor con este nombre")
+                }
+            }
+            catch {
+                throw; //aca paso a Program la excepcion que dio validate  de Proveedor
+            }
+
+        }
+
+        public Actividad BuscarActividad(string nombre,DateTime fecha) { //busco la actividad para validar cuando hago el alta
+            Actividad act = null;
+            bool existe = false;
+            int i = 0;
+
+            while (int i < _actividades.Count && existe == false) { //lo hago con un while para que no recorra innecesariamente
+                if (_actividades[i].Nombre == nombre && _actividades[i].Fecha == fecha) {
+                    existe = true;
+                    act = _actividades[i];
+                }
+            }
+
+            return act;
+
+        }
+
+        //revisar si la validacion de existencia es correcta
+
+        public void AltaActividadPropia(string nombre, string descripcion, DateTime fecha, int cantMaxPer, int edadMinima, decimal costo, int cupos, string responsable, string lugar, bool exterior) { //aca se va a llamar a los metodos correspondientes para la subclase ActividadPropia
+            ActividadPropia actividadPropia = new ActividadPropia(nombre,descripcion,fecha,cantMaxPer,edadMinima,costo,cupos,responsable,lugar,exterior);
+            try {
+                actividadPropia.Validate();
+                Actividad act = BuscarActividad(nombre, fecha); //lo hago con actvidad porque reviso si existe una actividad (no importa el tipo) con los mismos datos "clave"
+                if (act != null) {
+                    _actividades.Add(actividadPropia);
+                } else {
+                    throw new Exception("Ya existe una Actividad con este nombre") //puedo hacer el add porque es clase hija
+                }
+            } catch {
+                throw; //aca paso a Program la excepcion que dio validate de actividadPropia
+            }
+        }
+
+        //revisar si la validacion de existencia es correcta
+
+        //
+
+        public void AltaActividadTerciarizada(string nombre, string descripcion, DateTime fecha, int cantMaxPer, int edadMinima, decimal costo, int cupos, string nombreProveedor, bool confirmada, DateTime fechaConfirmacion) {//aca se va a llamar a los metodos correspondientes para la subclase ActividadTerciarizada
+            //antes de todo buscamos al proveedor
+            Proveedor prov = BuscarProveedor(nombreProveedor);
+            if (prov != null) { //si no hay proveedor no puedo crear la actividad terciarizada por la agregacion que esta en UML
+                //aca le paso el prov que encontre
+                ActividadTerciarizada actividadTerciarizada = new ActividadTerciarizada(nombre,descripcion,fecha,cantMaxPer,edadMinima,costo,cupos,prov,confirmada,fechaConfirmacion)
+                try {
+                        actividadTerciarizada.Validate();
+                        Actividad act = BuscarActividad(nombre, fecha); //lo hago con actvidad porque reviso si existe una actividad (no importa el tipo) con los mismos datos "clave"
+                        if (act != null) {
+
+                            _actividades.Add(actividadTerciarizada); //puedo hacer el add porque es clase hija
+                        }
+                        else {
+                            throw new Exception("Ya existe una Actividad con este nombre")
+                        }
+                 }
+                catch {
+                    throw; //aca paso a Program la excepcion que dio validate de actividadPropia
+                }
+            } else { 
+                throw new Exception("No existe un proveedor con ese nombre")
+            }
+        }
+
+        public void altaAgenda() {
+
+        }
 
     }
 }
