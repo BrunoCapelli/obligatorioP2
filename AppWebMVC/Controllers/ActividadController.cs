18b22a0b3f4d8d1ec5eb9179f3a7778d4c55a36e
Bruno Capelli <brunoicapelli@gmail.com>
2023-06-03 19:26:19 -0300

    Def Controllers

---
+++ AppWebMVC/Controllers/ActividadController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppWebMVC.Controllers
+{
+    public class ActividadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
